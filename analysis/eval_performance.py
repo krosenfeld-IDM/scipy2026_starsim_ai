b9f75b2e7fa561be1b4794c9eb8f9158e00f22ed
Cliff Kerr <questionably@gmail.com>
2026-02-25 01:17:13 -0500

    working on analysis

--- analysis/eval_performance.py
+++ analysis/eval_performance.py
@@ -1,3 +1,7 @@
+"""
+Evaluate performance of different models/configurations.
+"""
+
 # %%
 from rich import print
 
@@ -6,7 +10,7 @@ import matplotlib.pyplot as plt
 from inspect_ai.analysis import evals_df, samples_df
 
 df_eval = evals_df('../logs')
-df_samples = samples_df('../logs')
+# df_samples = samples_df('../logs')
 
 # %%
 print(df_eval.columns)
@@ -17,20 +21,20 @@ print(f"df_eval.shape: {df_eval.shape}")
 task_names = df_eval['task_name'].unique()
 print(task_names)
 # %%
-# Figure 1: Pass rate vs. allowed time to solve per problem
-task_name = 'starsim_agent_benchmark'
-df_task = df_eval[df_eval['task_name'] == task_name]
-df_task = df_task.sort_values(by='task_arg_request_timeout')
+# # Figure 1: Pass rate vs. allowed time to solve per problem
+# task_name = 'starsim_agent_benchmark'
+# df_task = df_eval[df_eval['task_name'] == task_name]
+# df_task = df_task.sort_values(by='task_arg_request_timeout')
 
-fig = plt.figure(figsize=(5,3))
-ax = plt.gca()
-ax.bar(np.arange(len(df_task)),df_task['score_headline_value'], color='xkcd:blue')
-ax.set_xticks(np.arange(len(df_task)))
-ax.set_xticklabels(df_task['task_arg_request_timeout'])
-ax.set_ylim(0, 1)
-ax.set_xlabel('Allowed time to solve per problem (seconds)')
-ax.set_ylabel('Pass rate')
-ax.set_title('Starsim Agent Benchmark')
+# fig = plt.figure(figsize=(5,3))
+# ax = plt.gca()
+# ax.bar(np.arange(len(df_task)),df_task['score_headline_value'], color='xkcd:blue')
+# ax.set_xticks(np.arange(len(df_task)))
+# ax.set_xticklabels(df_task['task_arg_request_timeout'])
+# ax.set_ylim(0, 1)
+# ax.set_xlabel('Allowed time to solve per problem (seconds)')
+# ax.set_ylabel('Pass rate')
+# ax.set_title('Starsim Agent Benchmark')
 
 # %%
 task_name = 'starsim_benchmark'
