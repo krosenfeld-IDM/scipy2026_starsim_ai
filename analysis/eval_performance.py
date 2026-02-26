54e9b557052e8b8a0a285e48ce14790b9246a690
Katherine Rosenfeld <krosenf@gmail.com>
2026-02-26 07:08:27 -0800

    Add seaborn dependency and update performance evaluation script
    - Added `seaborn` as a dependency in `pyproject.toml` and `uv.lock` for enhanced visualization capabilities.
    - Updated `eval_performance.py` to utilize `seaborn` for generating bar plots, improving the clarity and aesthetics of performance metrics visualizations.
    - Modified evaluation figures to include better labeling and sorting for improved readability.

--- analysis/eval_performance.py
+++ analysis/eval_performance.py
@@ -2,6 +2,7 @@
 from rich import print
 
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 from inspect_ai.analysis import evals_df, samples_df
 
@@ -17,32 +18,39 @@ print(f"df_eval.shape: {df_eval.shape}")
 task_names = df_eval['task_name'].unique()
 print(task_names)
 # %%
-# Figure 1: Pass rate vs. allowed time to solve per problem
+# Figure 1: Pass rate vs. allowed time to solve per problem for agent benchmark
 task_name = 'starsim_agent_benchmark'
 df_task = df_eval[df_eval['task_name'] == task_name]
+df_task = df_task[np.logical_not(df_task['task_arg_with_background'])]
 df_task = df_task.sort_values(by='task_arg_request_timeout')
+df_task = df_task.rename(columns={'task_arg_agent_url': 'w/ Plugin', 'task_arg_request_timeout': 'Time limit'})
+df_task = df_task.replace({'w/ Plugin': {'http://localhost:9100': 'No', 'http://localhost:9101': 'Yes'}})
 
 fig = plt.figure(figsize=(5,3))
 ax = plt.gca()
-ax.bar(np.arange(len(df_task)),df_task['score_headline_value'], color='xkcd:blue')
-ax.set_xticks(np.arange(len(df_task)))
-ax.set_xticklabels(df_task['task_arg_request_timeout'])
+sns.barplot(x='Time limit', y='score_headline_value', data=df_task, ax=ax, hue='w/ Plugin')
+for container in ax.containers:
+    ax.bar_label(container, fontsize=10, fmt='%.2f');
 ax.set_ylim(0, 1)
 ax.set_xlabel('Allowed time to solve per problem (seconds)')
 ax.set_ylabel('Pass rate')
 ax.set_title('Starsim Agent Benchmark')
 
 # %%
+# Figure 2: Pass rate vs. model for LLM benchmark
 task_name = 'starsim_benchmark'
 df_task = df_eval[df_eval['task_name'] == task_name]
 df_task = df_task.sort_values(by='model')
 
 fig = plt.figure(figsize=(5,3))
 ax = plt.gca()
-ax.bar(np.arange(len(df_task)),df_task['score_headline_value'], color='xkcd:lightblue')
-ax.set_xticks(np.arange(len(df_task)))
-ax.set_xticklabels(df_task['model'], rotation=45)
+sns.barplot(x='model', y='score_headline_value', data=df_task, ax=ax, hue='task_arg_with_background')
+for container in ax.containers:
+    ax.bar_label(container, fontsize=10, fmt='%.2f');
+ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
 ax.set_ylim(0, 1)
 ax.set_xlabel('Model')
 ax.set_ylabel('Pass rate')
 ax.set_title('Starsim LLM Benchmark')
+
+# %%
