b7aad6ae5f5bf23efb24aab9233711d14a1999fd
Cliff Kerr <questionably@gmail.com>
2026-03-05 17:24:24 -0500

    update docker command

--- docker_up.sh
+++ docker_up.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # Run this to start the Docker servers
-docker compose up --build --remove-orphans 2>&1 | tee container_output.log
+#   --log    Save output to container_output.log (disables terminal color)
+
+if [[ "$1" == "--log" ]]; then
+    docker compose up --build --remove-orphans 2>&1 | tee container_output.log
+else
+    docker compose up --build --remove-orphans
+fi
